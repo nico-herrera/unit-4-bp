f70fb9be97c0bcb6b72397d3081a9786e068b441
Nico Herrera <nherrera0831@gmail,com>
2021-04-18 21:55:20 -0700

    yes

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -12,6 +12,7 @@ async function add(user) {
     try {
         const [id] = await db('users').insert(user);
     console.log(id)
+    console.log("user model")
     return findById(id);
     } catch (err) {
         console.log(err)
